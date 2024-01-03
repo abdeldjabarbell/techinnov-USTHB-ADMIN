1971c77babbfe55925966d0d3f73c73016f54d2c
djebar <82457196+abdeldjabarbell@users.noreply.github.com>
2024-01-03 02:20:41 +0100

    Update ADMIN_home.js

--- ADMIN_home.js
+++ ADMIN_home.js
@@ -53,3 +53,48 @@ auth.onAuthStateChanged((user) => {
    home_user_login_filed.style.display="block";
    }
 });
+
+
+
+
+
+
+
+
+        //nav bar
+        let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+//nav bar scroll 
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let heihgt = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + heihgt){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+
+            });
+
+        };
+
+    });
+
+
+
+let header = document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY > 100);
+}
